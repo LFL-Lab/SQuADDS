fe1e14d92ee893d01cb789b526bc7a3a9657f588
Sadman Ahmed Shanto <shanto@usc.edu>
2024-07-04 22:04:30 -0700

    chore: updated the version + added release logs.

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'SQuADDS'
 copyright = '2023, Sadman Ahmed Shanto & Eli Levenson-Falk'
 author = 'Sadman Ahmed Shanto'
-release = "0.2.33"
+release = "0.2.34"
 
 # General configuration
 exclude_patterns = [
