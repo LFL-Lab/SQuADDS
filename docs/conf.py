d58359f0c9e56597c6902fb870397c4a0f791360
Sadman Ahmed Shanto <shanto@usc.edu>
2024-07-08 14:57:03 -0700

    docs: updated version for release with the most stable sim code.

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'SQuADDS'
 copyright = '2023, Sadman Ahmed Shanto & Eli Levenson-Falk'
 author = 'Sadman Ahmed Shanto'
-release = "0.2.35"
+release = "0.2.36"
 
 # General configuration
 exclude_patterns = [
--- squadds/__init__.py
+++ squadds/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '0.2.35'
+__version__ = '0.2.36'
 __license__ = "MIT License"
 __copyright__ = 'Sadman Ahmed Shanto, Eli Levenson-Falk 2023'
 __author__ = 'Sadman Ahmed Shanto, Eli Levenson-Falk'
