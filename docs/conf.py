b93f79a9acd31111aabda144efb59d783cd97fbc
GitHub Action <action@github.com>
2024-08-16 14:12:52 -0700

    docs: updating files to version 0.3.1

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'SQuADDS'
 copyright = '2023, Sadman Ahmed Shanto & Eli Levenson-Falk'
 author = 'Sadman Ahmed Shanto'
-release = "0.3.0"
+release = "0.3.1"
 
 # General configuration
 exclude_patterns = [
--- squadds/__init__.py
+++ squadds/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __license__ = "MIT License"
 __copyright__ = 'Sadman Ahmed Shanto, Eli Levenson-Falk 2023'
 __author__ = 'Sadman Ahmed Shanto, Eli Levenson-Falk'
