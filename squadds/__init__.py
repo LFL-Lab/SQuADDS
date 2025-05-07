6600080b6c8f291cfd5dcf0372f101babf75bc57
shanto268 <shanto@usc.edu>
2025-05-06 18:49:46 -0700

    docs: readying for next release

--- squadds/__init__.py
+++ squadds/__init__.py
@@ -1,9 +1,9 @@
 import os
 
-__version__ = '0.3.6'
+__version__ = '0.3.7'
 __license__ = "MIT License"
 __copyright__ = 'Sadman Ahmed Shanto, Eli Levenson-Falk 2023'
-__author__ = 'Sadman Ahmed Shanto, Eli Levenson-Falk'
+__author__ = 'Sadman Ahmed Shanto'
 __status__ = "Alpha"
 __repo_path__ = os.path.dirname(os.path.abspath(__file__))
 
