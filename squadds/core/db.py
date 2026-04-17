1395258604f9c26eb82bf36ddb6527f3094dc6b6
shanto268 <shanto@usc.edu>
2026-04-16 20:50:20 -0700

    refactor: extract qubit-cavity merge helper

--- squadds/core/db.py
+++ squadds/core/db.py
@@ -22,6 +22,7 @@ from squadds.core.db_catalog import (
     load_supported_config_names,
 )
 from squadds.core.db_loader import build_dataset_config, validate_dataset_request
+from squadds.core.db_merge import create_qubit_cavity_dataframe
 from squadds.core.db_selection import (
     build_component_selection,
     is_supported_coupler,
@@ -1214,36 +1215,13 @@ class SQuADDS_DB(metaclass=SingletonMeta):
         Raises:
             None
         """
-        for merger_term in merger_terms:
-            qubit_df[merger_term] = qubit_df["design_options"].map(
-                lambda x: x["connection_pads"]["readout"].get(merger_term)
-            )
-            cavity_df[merger_term] = cavity_df["design_options"].map(
-                lambda x: x["claw_opts"]["connection_pads"]["readout"].get(merger_term)
-            )
-
-        # Add index column to qubit_df
-        qubit_df = qubit_df.reset_index().rename(columns={"index": "index_qc"})
-
-        if parallelize:
-            n_cores = cpu_count() if num_cpu is None else num_cpu
-            qubit_df_splits = np.array_split(qubit_df, n_cores)
-
-            with Pool(n_cores) as pool:
-                merged_df_parts = list(
-                    tqdm(
-                        pool.starmap(merge_dfs, [(split, cavity_df, merger_terms) for split in qubit_df_splits]),
-                        total=n_cores,
-                    )
-                )
-
-            merged_df = pd.concat(merged_df_parts).reset_index(drop=True)
-        else:
-            merged_df = merge_dfs(qubit_df, cavity_df, merger_terms)
-
-        merged_df["design_options"] = merged_df.apply(create_unified_design_options, axis=1)
-
-        return merged_df
+        return create_qubit_cavity_dataframe(
+            qubit_df,
+            cavity_df,
+            merger_terms=merger_terms,
+            parallelize=parallelize,
+            num_cpu=num_cpu,
+        )
 
     def unselect_all(self):
         """
